095aaea14be426e7391544c0e90dfed145e36221
jwowillo <will.orourke10@gmail.com>
2017-04-12 15:25:18 -0500

    Resolve #1

--- game/rules.go
+++ game/rules.go
@@ -1,21 +1,31 @@
 package game
 
+import "time"
+
 // Rules encapsulates the variable parts of the game such as how many Pieces are
 // involved, how much damage each Piece can do, how much life they have, and how
 // much these increase when they destroy enemy Pieces.
 type Rules struct {
+	timerDuration                                          time.Duration
 	pieceCount, damage, life, damageIncrease, lifeIncrease int
 }
 
 // NewRules creates Rules with the given values for the variable parts.
-func NewRules(pc, d, l, di, li int) Rules {
+func NewRules(td time.Duration, pc, d, l, di, li int) Rules {
 	return Rules{
+		timerDuration:  td,
 		pieceCount:     pc,
 		damage:         d,
 		life:           l,
 		damageIncrease: di,
 		lifeIncrease:   li,
 	}
+}
+
+// TimerDuration is the duration for the timer that limits the duration of each
+// turn.
+func (r Rules) TimerDuration() time.Duration {
+	return r.timerDuration
 }
 
 // PieceCount specified by the Rules.
@@ -49,4 +59,4 @@ func (r Rules) LifeIncrease() int {
 }
 
 // StandardRules ...
-var StandardRules = NewRules(5, 1, 3, 1, 1)
+var StandardRules = NewRules(30*time.Second, 5, 1, 3, 1, 1)
